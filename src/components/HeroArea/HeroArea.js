dfca46deab8456b09f4a947c6896f10362321424
Voina Andra Maria <vandramaria@yahoo.com>
2020-04-03 23:15:11 +0300

    hero area update

--- src/components/HeroArea/HeroArea.js
+++ src/components/HeroArea/HeroArea.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import './HeroArea.css';
 import PhonesImg from '../../assets/Phones.svg';
+import chevroDownImg from '../../assets/chevron-down-solid.svg';
 
 const Phones = () => <img className="hero-image" src={PhonesImg} alt="phones img" />
-
+const ChevronDown = () => <img className="chevron-down" src={chevroDownImg} alt ="go down"/>
 export default class HeroArea extends React.Component {
     render() {
         return (
             <div className="hero-area-container">
-                <div className="left-panel">
-                    <h1 className="main-heading">
-                        <div>sigur,</div>
-                        <div>rapid &</div>
-                        <div>distractiv</div>
-                    </h1>
-                    <h3 className="secondary-heading">Lasă în grija noastră transportul copilului tău!</h3>
-                    <div className="button-container">
-                        <button className="cta" onClick={() => console.log('link catre playstore')}>Descarcă acum</button>
+                <div className="panel">
+                    <div className="left-panel">
+                        <h1 className="main-heading">
+                            <div>sigur,</div>
+                            <div>rapid&</div>
+                            <div>distractiv</div>
+                        </h1>
+                        <h3 className="secondary-heading">Lasă în grija noastră transportul copilului tău!</h3>
+                        <div className="button-container">
+                            <button className="cta" onClick={() => console.log('link catre playstore')}>Descarcă acum</button>
+                        </div>
+                    </div>
+                    <div className="right-panel">
+                        <Phones />
                     </div>
                 </div>
-                <div className="right-panel">
-                    <Phones />
+                <div className="border-bottom">
+                    <ChevronDown />
                 </div>
             </div>
         )
