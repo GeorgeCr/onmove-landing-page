aa8e168beffae257b68fd69c41c7db1df0ab303f
Voina Andra Maria <vandramaria@yahoo.com>
2020-04-04 00:24:53 +0300

    about us update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './index.css';
 import Header from './components/Header/Header';
 import HeroArea from './components/HeroArea/HeroArea';
+import AboutArea from './components/AboutArea/AboutArea'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HeroArea />
+      <AboutArea />
     </div>
   );
 }
