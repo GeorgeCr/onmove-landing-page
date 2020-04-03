bb03ff199a546da7afaae94ea5ddd102651aff5d
Voina Andra Maria <vandramaria@yahoo.com>
2020-04-04 02:57:06 +0300

    adding all page sections

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './index.css';
 import Header from './components/Header/Header';
 import HeroArea from './components/HeroArea/HeroArea';
-import AboutArea from './components/AboutArea/AboutArea'
+import AboutArea from './components/AboutArea/AboutArea';
+import BenefitsArea from './components/BenefitsArea/BenefitsArea';
+import ServicesArea from './components/ServicesArea/ServicesArea';
+import FeedbackArea from './components/FeedbackArea/FeedbackArea';
+import ContactArea from './components/ContactArea/ContactArea';
+
 
 function App() {
   return (
@@ -10,6 +15,12 @@ function App() {
       <Header />
       <HeroArea />
       <AboutArea />
+      <BenefitsArea />
+      <ServicesArea />
+      <FeedbackArea />
+      <ContactArea />
+      <div className="fucking-line">
+      </div>
     </div>
   );
 }
---
+++ src/components/ServicesArea/ServicesArea.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ServicesArea.css';
+
+export default class BenefitsArea extends React.Component {
+    render(){
+        return(
+            <div className="services-area-container">
+               <h2 className="area-title">tipuri de servicii</h2>
+               <div className="services-list">
+
+               </div>
+                
+           </div> 
+        )
+    }
+
+}
